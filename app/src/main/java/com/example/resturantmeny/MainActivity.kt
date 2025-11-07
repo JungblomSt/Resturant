a2b4fd70525a3039b20e6b0f1cabfc4f3d2f87ef
Stina <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-07 02:38:51 +0100

    added menuLists

--- app/src/main/java/com/example/resturantmeny/MainActivity.kt
+++ app/src/main/java/com/example/resturantmeny/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val menu1 = mutableListOf("Surkorv", "Kött på pinne", "Gravlaxrulle")
+    private val menu2 = mutableListOf("1/2 Kyckling","Riddarrester","Betbiffar")
+    private val menu3 = mutableListOf("Äppelkaka","Päron","Saffranspannkaka")
+
+    private val itemlist = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
