fd8eb55422e2a174138a35a9b74dd3c47074a06c
Stina <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-07 03:41:38 +0100

    entered buttons

--- app/src/main/java/com/example/resturantmeny/MainActivity.kt
+++ app/src/main/java/com/example/resturantmeny/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.resturantmeny
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+
+    private lateinit var listViewAdapter: ArrayAdapter<String>
 
     private val menu1 = mutableListOf("Surkorv", "Kött på pinne", "Gravlaxrulle")
     private val menu2 = mutableListOf("1/2 Kyckling","Riddarrester","Betbiffar")
@@ -18,6 +28,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        button1 = findViewById(R.id.button_menu1)
+        button2 = findViewById(R.id.button_menu2)
+        button3 = findViewById(R.id.button_menu3)
+
+        listView = findViewById(R.id.lv_menu)
+
+        listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itemlist)
+        listView.adapter = listViewAdapter
+
+        button1.setOnClickListener {
+            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu1)
+            listView.adapter = listViewAdapter
+        }
+        button2.setOnClickListener {
+            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu2)
+            listView.adapter = listViewAdapter
+        }
+        button3.setOnClickListener {
+            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu3)
+            listView.adapter = listViewAdapter
+        }
+
 
 
     }
