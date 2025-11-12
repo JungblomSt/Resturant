7bbd6c5f2a607da4c4d44997906569da42919e8e
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-12 17:49:39 +0100

    erased list

--- app/src/main/java/com/example/resturantmeny/MainActivity.kt
+++ app/src/main/java/com/example/resturantmeny/MainActivity.kt
@@ -14,19 +14,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    // =========== VARIABLES ==============
-    private lateinit var listView: ListView
     private lateinit var button1: Button
     private lateinit var button2: Button
     private lateinit var button3: Button
 
-    private lateinit var listViewAdapter: ArrayAdapter<String>
-
     private val menu1 = mutableListOf("Surkorv", "Kött på pinne", "Gravlaxrulle")
     private val menu2 = mutableListOf("1/2 Kyckling","Riddarrester","Betbiffar")
     private val menu3 = mutableListOf("Äppelkaka","Päron","Saffranspannkaka")
-
-    private val itemlist = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,21 +32,14 @@ class MainActivity : AppCompatActivity() {
         button3 = findViewById(R.id.button_menu3)
 
 
-
-        listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itemlist)
-        listView.adapter = listViewAdapter
-
         button1.setOnClickListener {
-            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu1)
-            listView.adapter = listViewAdapter
+
         }
         button2.setOnClickListener {
-            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu2)
-            listView.adapter = listViewAdapter
+
         }
         button3.setOnClickListener {
-            listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu3)
-            listView.adapter = listViewAdapter
+
         }
 
 
